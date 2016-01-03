105ed53f058f2e72ad3a0df68c37fda03d234339
Anita Amini <baadge29@yahoo.co.uk>
2016-01-03 19:28:47 +0000

    added some extra bits

--- multiboot_header.asm
+++ multiboot_header.asm
@@ -2,4 +2,12 @@ header_start:
     dd 0xe85250d6                ; arbitrary magic number required at start of multiboot. dd=define double word.
     dd 0                         ; protected mode code
     dd header_end - header_start ; header length
+    
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start)) 
+    
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
 header_end:
